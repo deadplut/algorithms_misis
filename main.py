9ace3ed223e6ed54013f8f198f561a07de252dbf
Suganov Boris <suganov@Diamond>
2024-04-03 23:18:57 +0300

    final

--- main.py
+++ main.py
@@ -1,12 +1,13 @@
-from algo_1_group import BubleSort, SimpleInsertsSort, DirectSelectSort, DwarfSort
-from algo_2group import PyramidSort, TournamentSort
-from algorithms import MergeSort, QuickSort
+from algo_1_group import BubleSort, SimpleInsertsSort, SimpleSelectSort, DwarfSort
+from algo_2group import PyramidSort, TournamentSort, ShellSort
+from algorithms import MergeSort, QuickSort, MSDSort, LSDSort
 
 output_file = 'results.txt'
-# a = MergeSort(output_file)
-a = QuickSort(output_file)
 
-a.run_tests()
+# a = ShellSort(output_file)
+#
+#
+# a.run_tests()
 
 
 
@@ -19,22 +20,39 @@ a.run_tests()
 ##################
 
 
-# a = BubleSort(output_file)
-# a = SimpleInsertsSort(output_file)
-# a = DirectSelectSort(output_file)
-# a = DwarfSort(output_file)
-
+# a = BubleSort('results_buble.txt')
+# a.run_tests()
+# b = SimpleInsertsSort('results_simple_inserts.txt')
+# b.run_tests()
+# # c = SimpleSelectSort(output_file)
+# c = SimpleSelectSort('results_simple_select.txt')
+# c.run_tests()
+# d = DwarfSort('results_dwarf.txt')
+# d.run_tests()
 
 
 ############################
 ## Вторая группа
 #############################
 
-# a = ShellSort(output_file)
-# a = PyramidSort(output_file)
-# a = TournamentSort(output_file)
+# a = ShellSort('results_Shell.txt')
+# a.run_tests()
+# b = PyramidSort('results_Pyrami.txt')
+# b.run_tests()
+# c= TournamentSort('results_Tournament.txt')
+# c.run_tests()
 
 ############################
 ## Третья группа
 #############################
 
+# a = MergeSort(output_file) #done
+# a.run_tests()
+#
+# a = QuickSort(output_file) #done
+# a.run_tests()
+
+a = MSDSort('results_MSDSort.txt')
+a.run_tests()
+b = LSDSort('results_LSDSort.txt')
+b.run_tests()
